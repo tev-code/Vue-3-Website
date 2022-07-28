e0e20a3340f7b4eb37dfe8c36a87f59057741e66
Tevin <you@example.com>
2022-07-28 21:33:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Header/>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400&display=swap');
+
+  :root { 
+  --header-height: 3.5rem;
+
+  /*========== Colors ==========*/
+  --first-color: #121212;
+  --button-color: #1F1A24;
+  --button-color-alt:#332940 ;
+  --title-color: #03DAC5;
+  --text-color: #5e526d;
+  --text-color-light: #BB86FC;
+  --body-color: #000000;
+  --container-color: #1a1a1a;
+  --border-color: #1F1B24;
+  
+
+    /*========== Font and typography ==========*/
+  --body-font: font-family: 'Poppins', sans-serif;
+  --biggest-font-size: 2rem;
+  --h1-font-size: 1.5rem;
+  --h2-font-size: 1.25rem;
+  --h3-font-size: 1rem;
+  --normal-font-size: .938rem;
+  --small-font-size: .813rem;
+  --smaller-font-size: .75rem;
+
+  /*========== Font weight ==========*/
+  --font-medium: 500;
+  --font-bold: 700;
+
+  /*========== Margenes Bottom ==========*/
+  --mb-0-5: .5rem;
+  --mb-0-75: .75rem;
+  --mb-1: 1rem;
+  --mb-1-5: 1.5rem;
+  --mb-2: 2rem;
+  --mb-2-5: 2.5rem;
+  --mb-3: 3rem;
+
+  /*========== z index ==========*/
+  --z-tooltip: 10;
+  --z-fixed: 100;
+}
+
+@media screen and (min-width: 968px) {
+  :root {
+    --biggest-font-size: 2.5rem;
+    --h1-font-size: 2.25rem;
+    --h2-font-size: 1.5rem;
+    --h3-font-size: 1.25rem;
+    --normal-font-size: 1rem;
+    --small-font-size: .875rem;
+    --smaller-font-size: .813rem;
+  }
+}
+
+/*=============== BASE ===============*/
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body,
+button,
+input {
+  font-family: var(--body-font);
+  font-size: var(--normal-font-size);
+}
+
+body {
+  margin: 0;
+  background-color: var(--body-color);
+  color: var(--text-color);
+  /*For animation dark mode*/
+  transition: .4s;
+}
+
+h1, h2, h3, h4 {
+  color: var(--title-color);
+  font-weight: var(--font-medium);
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+button {
+  cursor: pointer;
+  border: none;
+  outline: none;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,153 @@
+<script setup>
+
+</script>
+
+<template>
+ <header class="header" id="header">
+ <nav class="nav container">
+ <a href="#" class="nav__logo">
+ <i class="bx bxs-watch nav__logo-icon"></i> Rolex
+ </a>
+ 
+ <div class="nav__menu" id="nav-menu">
+ <ul class="nav__list">
+ <li class="nav__item">
+ <a href="#home" class="nav__link active-link">Home</a>
+ </li>
+ <li class="nav__item">
+ <a href="#featured" class="nav__link">Featured</a>
+ </li>
+ <li class="nav__item">
+ <a href="#products" class="nav__link">Products</a>
+ </li>
+ <li class="nav__item">
+ <a href="#new" class="nav__link">New</a>
+ </li>
+ </ul>
+ 
+ <div class="nav__close" id="nav-close">
+ <i class="bx bx-x"></i>
+ </div>
+ </div>
+
+ <div class="nav__btns">
+ <i class="bx bx-moon change-theme"></i>
+
+ <div class="nav__shop" id="cart-shop">
+ <i class="bx bx-shopping-bag"></i>
+ </div>
+
+ <div class="nav__toggle" id="nav-toggle">
+ <i class="bx bx-grid-alt"></i>
+ 
+ </div>
+ </div>
+ </nav>
+ 
+ </header>
+</template>
+
+<style scoped>
+.header {
+    width: 100%;
+    background: transparent;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: var(--z-fixed);
+    /*For animation dark mode*/
+    transition: .4s;
+}
+
+.nav {
+    height: var(--header-height);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.nav__logo, 
+.nav__toggle,
+.nav__shop,.nav__close {
+  color: var(--title-color);
+}
+
+.nav__logo {
+    text-transform: uppercase;
+    font-weight: var(--font-bold);
+    letter-spacing: 1px;
+    display: inline-flex;
+    align-items: center;
+    column-gap: .25rem;
+}
+
+.nav__logo-icon {
+    font-size: 1.25rem;
+}
+
+.nav__btns {
+    display: flex;
+    align-items: center;
+    column-gap: 1rem;
+}
+
+.nav__toggle,
+.nav__shop {
+    font-size: 1.25rem;
+    cursor: pointer;
+}
+
+@media screen and (max-width: 767px) {
+    .nav__menu {
+        position: fixed;
+        background-color: var(--body-color);
+        top: 0;
+        right: -100%;
+        width: 100%;
+        height: 100%;
+        padding: 6rem 2rem 3.5rem;
+        transition: .3s;
+        }
+}
+
+     .nav__list {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        row-gap: 2rem;
+     }
+
+     .nav__link {
+        color: var(--title-color);
+        font-weight: var(--font-medium);
+        text-transform: uppercase;
+        font-size: var(--h2-font-size);
+        transition: .3s;
+     }
+
+     .nav__link:hover {
+        color: var(--first-color);
+     }
+
+     .nav__close {
+        font-size: 2rem;
+        position: absolute;
+        top: .9rem;
+        right: 1.25rem;
+        cursor: pointer;
+     }
+
+     .show-menu {
+        right: 0;
+     }
+
+     .scroll-header {
+        background-color: var(--first-color-color);
+        box-shadow: 0 1px 4px hsla(0, 4%, 15%, .10);
+     }
+
+     .active-link {
+        color: var(--first-color);
+     }
+
+</style>
